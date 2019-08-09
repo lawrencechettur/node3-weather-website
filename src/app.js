60b5b76968fe039c648c7170920d0981a1631532
Lawrence Chettur <Lawrence_Chettur@ad.Infosys.com>
2019-08-09 17:53:59 +1000

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ console.log(__dirname)
 console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -112,6 +113,6 @@ app.get('/weather', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port + '.')
 })
